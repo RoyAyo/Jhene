f11e226f5791a54261799596b94f92f1e6cac4e8
RoyAyo <roylayindeay0@gmail.com>
2020-12-15 16:28:54 +0100

    added an initial page loader

--- src/components/component/Screen.js
+++ src/components/component/Screen.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import JheneImg from '../../static/back.png';
 
 const Screen = () => {
     return (
-        <div style={{display:'flex',height:'100%',width:'100%','justifyContent':'center','alignItems':'center'}}>
-            <h3>JHENE</h3>
+        <div style={{display:'flex',height:'100%',width:'100%',justifyContent:'center',alignItems:'center',background:'#3474E2'}}>
+            <img src={JheneImg} alt='' />
         </div>
     )
 }
