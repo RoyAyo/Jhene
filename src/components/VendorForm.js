776ea193da42e03f83f7f64efcb5bd7406ae43c7
RoyAyo <roylayindeay0@gmail.com>
2021-01-07 15:57:16 +0100

    did the vector add page

--- src/components/VendorForm.js
+++ src/components/VendorForm.js
@@ -2,22 +2,36 @@ import React from 'react';
 
 import Header from './component/Header';
 import Input from './component/Input';
+import "../static/css/vendor-form.css";
+import Footer from './component/Footer';
 
 const VendorForm = () => {
     return (
         <div>
             <Header />
             <div className="wrapper">
-                <h3>Request Vendor Access</h3>
-                <p>To get your business featured on this platform for free, you have to make a request so we can reach out to you as soon as possible and verify before we add you.</p>
-                <div className="apply-form">
-                    <Input name="Business Name" placeholder="Type your business name" type="text"/>
-                    <Input name="Business Name" placeholder="Type your email" type="email"/>
-                    <Input name="Business Name" placeholder="Type a link" type="url"/>
-                    <button className="submit-access">
-                        Submit
-                    </button>
+                <h3 className="req-ven">Request Vendor Access</h3>
+                <div className="info">
+                    <p>
+                        To get your business featured on this platform for free, you have to make a request so we can reach out to you as soon as possible and verify before we add you.
+                    </p>
                 </div>
+                <div className="apply-form">
+                    <div className="fill-form">
+                        <Input name="Business Name" placeholder="Type your business name" type="text"/>
+                        <Input name="Email" placeholder="Type your email" type="email"/>
+                        <Input name="Link to website or Social media" placeholder="Type a link" type="url"/>
+                        <div className="submit-access">
+                            <button>
+                                Submit
+                            </button>
+                        </div>
+                    </div>
+                    <div className="form-nothing">
+
+                    </div>
+                </div>
+                <Footer />
             </div>
         </div>
     )
