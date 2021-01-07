e67df58386550e87f2aaf03fae5c251168e95193
RoyAyo <roylayindeay0@gmail.com>
2021-01-07 15:12:51 +0100

    changed the logo

--- src/components/VendorForm.js
+++ src/components/VendorForm.js
@@ -1,11 +1,24 @@
-import React from 'react'
-import Header from './component/Header'
+import React from 'react';
 
+import Header from './component/Header';
+import Input from './component/Input';
 
 const VendorForm = () => {
     return (
         <div>
             <Header />
+            <div className="wrapper">
+                <h3>Request Vendor Access</h3>
+                <p>To get your business featured on this platform for free, you have to make a request so we can reach out to you as soon as possible and verify before we add you.</p>
+                <div className="apply-form">
+                    <Input name="Business Name" placeholder="Type your business name" type="text"/>
+                    <Input name="Business Name" placeholder="Type your email" type="email"/>
+                    <Input name="Business Name" placeholder="Type a link" type="url"/>
+                    <button className="submit-access">
+                        Submit
+                    </button>
+                </div>
+            </div>
         </div>
     )
 };
