49a98b33a632e11b77ab4b7b37d52412f3c84b4e
RoyAyo <roylayindeay0@gmail.com>
2021-01-31 19:36:30 +0100

    package changes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {BrowserRouter as Router, Route} from 'react-router-dom';
 import LandingPage from './components/LandingPage';
 import Register from './components/Register';
 import VendorForm from './components/VendorForm';
+import Speech from './components/Speech';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Route path='/register' exact component={Register} />
       <Route path='/vendor-form' exact component={VendorForm} />
       <Route path='/chat' exact component={ChatScreen} />
+      <Route path='/speech' exact component={Speech} />
     </Router>
   );
 }
