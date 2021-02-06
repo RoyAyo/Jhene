5afdc34181394b93130cfda7a69abd56889dee08
RoyAyo <46534625+RoyAyo@users.noreply.github.com>
2021-02-06 23:05:32 +0100

    Update VendorForm.js

--- src/components/VendorForm.js
+++ src/components/VendorForm.js
@@ -78,7 +78,7 @@ const VendorForm = () => {
                         <Input name="Business Name" placeholder="Type your business name" type="text" onChange={name => setName(name)}/>
                         <Input name="Email" placeholder="Type your email" type="email" onChange={email => setEmail(email)}/>
                         <Input name="Location" placeholder="State, Country" type="text" onChange={location => setLocation(location)}/>
-                        <Input name="Link to website" placeholder="Type a link" type="url" onChange={link => setLink(link)}/>
+                        <Input name="Link to website/Social Media" placeholder="Type a link" type="url" onChange={link => setLink(link)}/>
                         <Input name="Short description of what you do or sell" placeholder="I Sell Fashion Items" type="text" onChange={desc => setDescription(desc)}/>
                         <div className="submit-access">
                             <button onClick={submit}>
